a5adf0a70ccc1c1696e88f66acb84563ef63f320
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2023-01-17 12:48:46 +0000

    Updated usage

--- dash_wtgviewer/DashWtgviewer.py
+++ dash_wtgviewer/DashWtgviewer.py
@@ -11,241 +11,27 @@ Keyword arguments:
 
 - id (string; required)
 
-- model (dict; required)
+- model (optional)
 
-    `model` is a dict with keys:
+- sea (boolean; default True)
 
-    - blades (list of dicts; optional)
-
-        `blades` is a list of dicts with keys:
-
-        - id (string; optional)
-
-        - name (string; optional)
-
-        - node (dict; optional)
-
-            `node` is a dict with keys:
-
-            - id (number; optional)
-
-            - x (number; optional)
-
-            - y (number; optional)
-
-            - z (number; optional)
-
-        - url (string; optional)
-
-    - foundation (dict; optional)
-
-        `foundation` is a dict with keys:
-
-        - element_sets (list of dicts; optional)
-
-            `element_sets` is a list of dicts with keys:
-
-    - elements (list of dicts; optional)
-
-        `elements` is a list of dicts with keys:
-
-        - diameter (number; optional)
-
-        - eltype (string; optional)
-
-        - id (number; optional)
-
-        - nodes (list of dicts; optional)
-
-            `nodes` is a list of dicts with keys:
-
-            - id (number; optional)
-
-            - x (number; optional)
-
-            - y (number; optional)
-
-            - z (number; optional)
-
-        - thickness (number; optional)
-
-    - id (string; optional)
-
-    - name (string; optional)
-
-        - id (string; optional)
-
-        - name (string; optional)
-
-        - node_sets (list of dicts; optional)
-
-            `node_sets` is a list of dicts with keys:
-
-    - id (string; optional)
-
-    - name (string; optional)
-
-    - nodes (list of dicts; optional)
-
-        `nodes` is a list of dicts with keys:
-
-        - id (number; optional)
-
-        - x (number; optional)
-
-        - y (number; optional)
-
-        - z (number; optional)
-
-    - hub (dict; optional)
-
-        `hub` is a dict with keys:
-
-        - cone (dict; optional)
-
-            `cone` is a dict with keys:
-
-            - diameter (number; optional)
-
-            - eltype (string; optional)
-
-            - id (number; optional)
-
-            - nodes (list of dicts; optional)
-
-                `nodes` is a list of dicts with keys:
-
-    - id (number; optional)
-
-    - x (number; optional)
-
-    - y (number; optional)
-
-    - z (number; optional)
-
-        - id (string; optional)
-
-        - name (string; optional)
-
-    - id (string; optional)
-
-    - nacelle (dict; optional)
-
-        `nacelle` is a dict with keys:
-
-        - element_sets (list of dicts; optional)
-
-            `element_sets` is a list of dicts with keys:
-
-    - elements (list of dicts; optional)
-
-        `elements` is a list of dicts with keys:
-
-        - eltype (string; optional)
-
-        - height (number; optional)
-
-        - id (number; optional)
-
-        - nodes (list of dicts; optional)
-
-            `nodes` is a list of dicts with keys:
-
-            - id (number; optional)
-
-            - x (number; optional)
-
-            - y (number; optional)
-
-            - z (number; optional)
-
-        - width (number; optional)
-
-    - id (string; optional)
-
-    - name (string; optional)
-
-        - id (string; optional)
-
-        - name (string; optional)
-
-    - name (string; optional)
-
-    - tower (dict; optional)
-
-        `tower` is a dict with keys:
-
-        - element_sets (list of dicts; optional)
-
-            `element_sets` is a list of dicts with keys:
-
-    - elements (list of dicts; optional)
-
-        `elements` is a list of dicts with keys:
-
-        - diameters (list of numbers; optional)
-
-        - eltype (string; optional)
-
-        - id (number; optional)
-
-        - nodes (list of dicts; optional)
-
-            `nodes` is a list of dicts with keys:
-
-            - id (number; optional)
-
-            - x (number; optional)
-
-            - y (number; optional)
-
-            - z (number; optional)
-
-        - thicknesses (list of numbers; optional)
-
-    - id (string; optional)
-
-    - name (string; optional)
-
-        - id (string; optional)
-
-        - name (string; optional)
-
-        - node_sets (list of dicts; optional)
-
-            `node_sets` is a list of dicts with keys:
-
-    - id (string; optional)
-
-    - name (string; optional)
-
-    - nodes (list of dicts; optional)
-
-        `nodes` is a list of dicts with keys:
-
-        - id (number; optional)
-
-        - x (number; optional)
-
-        - y (number; optional)
-
-        - z (number; optional)"""
+- tooltip (boolean; default False)"""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'dash_wtgviewer'
     _type = 'DashWtgviewer'
     @_explicitize_args
-    def __init__(self, id=Component.REQUIRED, model=Component.REQUIRED, **kwargs):
-        self._prop_names = ['id', 'model']
+    def __init__(self, id=Component.REQUIRED, model=Component.UNDEFINED, tooltip=Component.UNDEFINED, sea=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'model', 'sea', 'tooltip']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'model']
+        self.available_properties = ['id', 'model', 'sea', 'tooltip']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
         _locals.update(kwargs)  # For wildcard attrs and excess named props
         args = {k: _locals[k] for k in _explicit_args}
 
-        for k in ['id', 'model']:
+        for k in ['id']:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
