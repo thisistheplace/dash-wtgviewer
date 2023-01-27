7ae15c779b1f171e81e704f10e49b57b70e93cc6
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2023-01-27 11:41:09 +0000

    Made example results more palatable

--- random_results.py
+++ random_results.py
@@ -1,5 +1,5 @@
 import json
-from random import random
+import math
 
 from dash_wtgviewer.model.model import Model, find_type
 from dash_wtgviewer.model.fea.elements import Element
@@ -8,10 +8,21 @@ from dash_wtgviewer.model.results.base import BaseResult
 from dash_wtgviewer.model.results.element import ElementResult
 from dash_wtgviewer.model.results.results import Results
 
+MAX_VAL = 100
+MIN_VAL = 20
+
 data = json.load(open("assets/ea1_model.json", "r"))
 model = Model(**data)
 elements = find_type(model, Element)
 print(f"found {len(elements)} elements")
+
+# find height difference
+max_height = -math.inf
+min_height = math.inf
+for element in elements:
+    max_height = max(max_height, element.nodes[0].z)
+    min_height = min(min_height, element.nodes[0].z)
+height = max_height - min_height
 
 element_results = []
 for element in elements:
@@ -20,7 +31,7 @@ for element in elements:
             target=element.id,
             results=[
                 BaseResult(
-                    value=random()
+                    value= MIN_VAL + (MAX_VAL - MIN_VAL) * (max_height - element.nodes[0].z) / height
                 )
                 for _ in element.nodes
             ]
